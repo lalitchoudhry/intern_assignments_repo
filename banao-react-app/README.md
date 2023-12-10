bf72382e2c95a3ac0d04b3fbb892e73e08c6f92f
Lalit <choudharylalit182@gmail.com>
2023-12-10 20:57:46 +0530

    fresh start for Across globe intern assignment

---
+++ banao-react-app/README.md
@@ -0,0 +1,3 @@
+# Getting Started with Create React App
+## Banao Reactjs Assignment K4
+Basic testing of responsivness
---
+++ banao-react-app/src/App.js
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      Hello react
+    </div>
+  );
+}
+
+export default App;
